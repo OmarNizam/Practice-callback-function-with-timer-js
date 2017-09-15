467d75fe1db9401d9e9011e15c55bb5063adb127
OmarNizam <eng.omarnizam@gmail.com>
2017-09-15 16:00:54 +0200

    use focus and burl with addEventListener

--- callbacks_and_the_DOM/Using_Callbacks_on_Text_Inputs/app.js
+++ callbacks_and_the_DOM/Using_Callbacks_on_Text_Inputs/app.js
@@ -1,6 +1,18 @@
 const nameInput = document.getElementById('name');
 const messageTextArea = document.getElementById('message');
 
-nameInput.addEventListener('click', event => {
+nameInput.addEventListener('focus', event => {
   event.target.className = 'highlight';
+});
+
+nameInput.addEventListener('blur', event => {
+  event.target.className = '';
+});
+
+messageTextArea.addEventListener('focus', event => {
+  event.target.className = 'highlight';
+});
+
+messageTextArea.addEventListener('blur', event => {
+  event.target.className = '';
 });

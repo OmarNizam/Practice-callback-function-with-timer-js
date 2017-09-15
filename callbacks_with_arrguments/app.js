8d8b1d85223e247d1426cf1cf0782ae97c0c81cd
OmarNizam <eng.omarnizam@gmail.com>
2017-09-15 14:10:53 +0200

    make color functions for the divs

---
+++ callbacks_with_arrguments/app.js
@@ -0,0 +1,16 @@
+
+const div1 = document.getElementById('first');
+const div2 = document.getElementById('second');
+const div3 = document.getElementById('third');
+
+function makeRed(element) {
+   element.style.backgroundColor = "red";
+}
+
+function makeBlue(element) {
+   element.style.backgroundColor = "blue";
+}
+
+function makeGreen(element) {
+  element.style.backgroundColor = "green";
+}
